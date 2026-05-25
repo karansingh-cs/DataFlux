c51c165eaf83d54d230bcd331c0f56e82d67e578
Karan <karanbir97@outlook.com>
2026-05-25 12:28:04 -0500

    Add pyads reader script to pull live PLC data

---
+++ python/read_plc.py
@@ -0,0 +1,24 @@
+import pyads
+import time
+
+# Connect to local TwinCAT runtime
+plc = pyads.Connection('192.168.1.10.1.1', pyads.PORT_TC3PLC1)
+plc.open()
+
+try:
+    while True:
+        # Read variables from MachineData GVL
+        conveyor_running = plc.read_by_name('MachineData.ConveyorRunning', pyads.PLCTYPE_BOOL)
+        motor_temp       = plc.read_by_name('MachineData.MotorTemp',       pyads.PLCTYPE_REAL)
+        part_count       = plc.read_by_name('MachineData.PartCount',       pyads.PLCTYPE_INT)
+
+        print(f'Conveyor Running : {conveyor_running}')
+        print(f'Motor Temp       : {motor_temp}')
+        print(f'Part Count       : {part_count}')
+        print('------------')
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print('Closed!')
+    plc.close()
